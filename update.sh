63e8aee3a2ad8bffac63196386a62fb9a0b48dee
TTTkong <52569535+TTTkong@users.noreply.github.com>
2024-09-27 16:34:27 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@ echo 获取链接，速度因网络而异
 wget -O ./data/config.yaml https://tt.vg/freeclash #此处填写订阅链接，然后记得检查订阅文件的外部端口是否为9090，很多都是
 
 echo 重启容器，刷新配置文件
-docker-compose up -d --force-recreate
+docker-compose down
+docker-compose up -d
 
 echo 以下是可能的ip地址或者公网地址
 ip addr
