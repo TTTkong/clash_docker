0dc4223f5f595970dfc497d71c8e538cbd834380
TTTkong <52569535+TTTkong@users.noreply.github.com>
2024-09-29 17:22:27 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 echo 获取配置文件，速度因网络而异
 wget -O ./data/config.yaml https://tt.vg/freeclash #此处填写订阅链接，然后记得检查订阅文件的外部端口是否为9090，很多都是
 
-echo 将混合端口设置为7777，若有需求请自行修改docker-compose和data/config.yaml文件
+echo 将混合端口设置为7777，并开启允许局域网访问，若有需求请自行修改docker-compose和data/config.yaml文件
 sed -i '/mixed-port/d' data/config.yaml
+sed -i '/allow-lan/d' data/config.yaml
+
 sed -i '1 i \mixed-port: 7777\n' data/config.yaml
+sed -i '1 i \allow-lan: true\n' data/config.yaml
 
 echo 重启容器，刷新配置文件
 docker-compose down
