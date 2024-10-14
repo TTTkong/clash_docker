21524e277d5bb1d14d90f3f5c43019bf630d264c
TTTkong <2020327889@qq.com>
2024-10-14 20:39:03 +0800

    分离了clash核心和ui

--- update.sh
+++ update.sh
@@ -2,13 +2,15 @@ echo 获取配置文件，默认的免费链接容易失效，建议自己寻找
 # 本地订阅链接转换服务获取，免费链接获取地址为  https://github.com/VPN-Subcription-Links/ClashX-V2Ray-TopFreeProxy
 # 以下指令自行替换链接
 # wget -O ./data/config.yaml https://tt.vg/freeclash #随时可能挂，建议自己找链接
-wget -O ./data/config.yaml "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/clash.yml" #随时可能挂，建议自己找链接
+# wget -O ./data/config.yaml "https://git.io/emzclash" #随时可能挂，建议自己找链接
 echo 将混合端口设置为7777，并开启允许局域网访问，若有需求请自行修改docker-compose和data/config.yaml文件
-sed -i '/mixed-port/d' data/config.yaml
-sed -i '/allow-lan/d' data/config.yaml
+# sed -i '/mixed-port/d' ./data/config.yaml
+# sed -i '/allow-lan/d' ./data/config.yaml
+# sed -i '/external-controller/d' ./data/config.yaml
 
-sed -i '1a mixed-port: 7777' data/config.yaml
-sed -i '1a allow-lan: true' data/config.yaml
+# sed -i '1a mixed-port: 7777' ./data/config.yaml
+# sed -i '1a allow-lan: true' ./data/config.yaml
+# sed -i '1a external-controller: "127.0.0.1:9090"' ./data/config.yaml
 
 echo 重启容器，刷新配置文件
 docker-compose restart clashs
